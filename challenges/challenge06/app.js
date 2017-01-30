98047ac8e860dda901604b6662d0c28b11037a6d
Apollo013 <paul.j.millar@gmail.com>
2017-01-30 11:52:50 +0000

    Completed Challenge 06

---
+++ challenges/challenge06/app.js
@@ -0,0 +1,19 @@
+var express = require('express'), 
+    http = require('http'),
+    path = require('path'),
+    browserify = require('browserify-middleware');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configure Browserify middleware
+var browserifySrc = path.join(process.cwd(), 'browser.js');
+// TODO: How do you use browserify-middleware to create
+// the "/middleware.js" file for the browser?
+app.use('/middleware.js', browserify(browserifySrc));
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
